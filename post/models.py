1db584270a14e88783a82d773219891c279f6747
ARGEN03 <argen.arzykulov03@gmail.com>
2024-01-08 20:59:44 +0600

    add Post

---
+++ post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.Text()
+    # category 
+    # image
+    image = models.ImageField(upload_to='images/', null=True)
+    # owner 
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
