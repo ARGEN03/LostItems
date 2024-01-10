f247fda92f13243b2026a6cff8970275fd443f31
ARGEN03 <argen.arzykulov03@gmail.com>
2024-01-10 18:44:15 +0600

    я доработаль пост и категории

--- Comment/models.py
+++ Comment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-# from post.models import Post
+from post.models import Post
+from account.models import CustomUser
 
 # Create your models here.
 
@@ -7,13 +8,13 @@ from django.db import models
 class Comment(models.Model):
     content = models.TextField()
     owner = models.ForeignKey(
-        'auth.User',
+        CustomUser,
         on_delete = models.CASCADE,
         related_name = 'comments'
     )
     created_at = models.DateField(auto_now_add = True)
     post = models.ForeignKey(
-        # Post,
+        Post,
         on_delete = models.CASCADE,
         related_name = 'comments'
     )
--- post/models.py
+++ post/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from Category.models import Category
+from account.models import CustomUser
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     desc = models.TextField()
-    # category 
-    # image
+    category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='images/', null=True)
-    # owner 
+    owner = models.ForeignKey(CustomUser,related_name='posts', on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
