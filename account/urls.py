b30ee1284c03221c22d3a4250317a92754038969
Bagyshan <bagishan01@gmail.com>
2024-01-13 14:44:34 +0600

    .

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('user_detail/<int:user_id>/', UserDetail.as_view()),
-    path('get_users/', UserListAPIView.as_view()),
+    path('', UserListAPIView.as_view()),
     path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
--- category/views.py
+++ category/views.py
@@ -5,9 +5,11 @@ from category import permissions
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework import permissions
-from .permissions import ISstaff
+from .permissions import IsStaff
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 class CategoryViewSet(ModelViewSet):
@@ -16,8 +18,12 @@ class CategoryViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['name']
 
+    @method_decorator(cache_page(60 * 15))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_permissions(self):
         if self.request.method in ['PATCH', 'PUT', 'DELETE','POST']:
-            return [permissions.ISstaff()]
+            return [IsStaff()]
         return [permissions.AllowAny()]
     
